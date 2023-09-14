f53741a0fd61fe7db25cd1ef01bad808a3d1a0c1
GabrielSampaioS <89399378+GabrielSampaioS@users.noreply.github.com>
2023-09-14 15:59:41 -0300

    Update SQL_PKeFK.sql

--- SQL_PKeFK.sql
+++ SQL_PKeFK.sql
@@ -1,36 +1,31 @@
--- Buscar as chaves prim·rias da tabela
-select DISTINCT t.name as 'Tabela',
-	    P.name 'NomeColuna',
-	   'PK' as 'Tipo',
-	   kc.name as 'NomeChave',
-	   '-' as 'Origem'
-from sys.all_columns ac
-inner join sys.columns c on ac.object_id = c.object_id
-inner join sys.tables t on c.object_id = t.object_id
-inner join sys.key_constraints kc on t.object_id = kc.parent_object_id
-inner join sys.indexes i on t.object_id = i.object_id
-inner join (select object_id as 'ID' , name from sys.indexes where is_primary_key = 1) P on P.ID = t.object_id
+-- Buscar as chaves prim√°rias da tabela
+SELECT t.name AS 'Tabela', 
+       c.name AS 'NomeColuna',
+       'PK' AS 'Tipo',
+       i.name AS 'NomeChave',
+       '-' AS 'Origem'
+FROM sys.indexes i
+INNER JOIN sys.tables t ON t.object_id = i.object_id
+INNER JOIN sys.index_columns ic ON ic.object_id = t.object_id AND ic.index_id = i.index_id
+INNER JOIN sys.columns c ON c.object_id = t.object_id AND c.column_id = ic.column_id
+WHERE i.is_primary_key = 1
 UNION
 -- Buscar as chaves estrangeiras da tabela
-SELECT t.name as 'Tabela',
-	   c.name  'NomeColuna',
-	   'FK' as 'Tipo',
-	   FK.name as 'NomeChave',
-	   Origem.Origem
-FROM 
-    sys.foreign_key_columns AS fkc
-JOIN 
-    sys.all_columns AS ac ON fkc.parent_object_id = ac.object_id 
-                           AND fkc.parent_column_id = ac.column_id
-JOIN
-    sys.all_columns AS rc ON fkc.referenced_object_id = rc.object_id
-                           AND fkc.referenced_column_id = rc.column_id
-inner join sys.tables t on fkc.parent_object_id = t.object_id
-inner join sys.foreign_keys fk on fkc.constraint_object_id = fk.object_id
-inner join sys.columns c on t.object_id = c.object_id and FKC.parent_column_id = c.column_id 
-inner join (
-			SELECT rt.name as 'Origem', fk.name
-			FROM sys.foreign_keys AS fk
-			INNER JOIN sys.tables AS pt ON fk.parent_object_id = pt.object_id
-			INNER JOIN sys.tables AS rt ON fk.referenced_object_id = rt.object_id) Origem on fk.name = Origem.name
-order by Tabela Asc, Tipo Desc
+SELECT t.name AS 'Tabela',
+	   c.name AS 'NomeColuna',
+	   'FK' AS 'Tipo', 
+	   NTO.NomeChave AS 'NomeChave',
+	   NTO.Tabela AS 'Origem'
+FROM sys.tables t
+INNER JOIN sys.foreign_key_columns FK ON t.object_id = FK.parent_object_id
+INNER JOIN sys.columns c ON t.object_id = c.object_id AND FK.parent_column_id = c.column_id 
+INNER JOIN (
+    SELECT c.name AS 'Campo', t.name AS 'Tabela', FK.name AS 'NomeChave'
+    FROM sys.foreign_keys FK
+    INNER JOIN sys.foreign_key_columns FKC ON FK.object_id = FKC.constraint_object_id
+    INNER JOIN sys.tables t ON FK.referenced_object_id = t.object_id
+    INNER JOIN sys.index_columns ic ON t.object_id = ic.object_id 
+    INNER JOIN sys.columns c ON t.object_id = c.object_id AND ic.column_id = c.column_id
+    INNER JOIN sys.indexes i ON t.object_id = i.object_id AND ic.index_id = i.index_id
+) AS NTO ON NTO.Campo = c.name 
+ORDER BY 'Tabela' ASC, 'Tipo' DESC;
